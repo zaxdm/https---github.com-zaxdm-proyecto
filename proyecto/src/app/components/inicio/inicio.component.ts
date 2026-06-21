c53597e9999f8fcc6982da51bfc61a8447c8702b
Shoy09 <jhoel.dioses@tecsup.edu.pe>
2026-06-21 17:28:14 -0500

    fondos

--- proyecto/src/app/components/inicio/inicio.component.ts
+++ proyecto/src/app/components/inicio/inicio.component.ts
@@ -5,19 +5,50 @@ import { Component } from '@angular/core';
   selector: 'app-inicio',
   imports: [CommonModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrl: './inicio.component.css',
 })
 export class InicioComponent {
-
-productos = [
-    { nombre: 'Brocas de Perforación', imagen: 'assets/images/pro_01.jpg', link: '/productos/brocas' },
-    { nombre: 'Barras de Perforación', imagen: 'assets/images/pro_02.jpg', link: '/productos/barras' },
-    { nombre: 'Adaptadores de Culata', imagen: 'assets/images/pro_03.jpg', link: '/productos/adaptadores' },
-    { nombre: 'Herramientas DTH', imagen: 'assets/images/pro_06.jpg', link: '/productos/dth' },
-    { nombre: 'Herramientas Cónicas', imagen: 'assets/images/pro_07.jpg', link: '/productos/conicas' },
-    { nombre: 'Bulones de Anclaje', imagen: 'assets/images/pro_05.jpg', link: '/productos/anclajes' },
-    { nombre: 'Herramientas de Piqueras', imagen: 'assets/images/pro_08.jpg', link: '/productos/piqueras' },
-    { nombre: 'Otros Productos', imagen: 'assets/images/pro_04.jpg', link: '/productos/otros' },
+  productos = [
+    {
+      nombre: 'Sombreros Artesanales',
+      imagen: '/producto/sombrero.png',
+      link: '/productos/sombreros',
+    },
+    {
+      nombre: 'Lentes Artesanales',
+      imagen: '/producto/lente.png',
+      link: '/productos/lentes',
+    },
+    {
+      nombre: 'Sandalias de Coco',
+      imagen: '/producto/zandalias.png',
+      link: '/productos/sandalias',
+    },
+    {
+      nombre: 'Maceteros Decorativos',
+      imagen: '/producto/macetero.png',
+      link: '/productos/maceteros',
+    },
+    {
+      nombre: 'Llaveros Personalizados',
+      imagen: '/producto/llavero.png',
+      link: '/productos/llaveros',
+    },
+    // {
+    //   nombre: 'Souvenirs Turísticos',
+    //   imagen: 'assets/images/souvenirs.jpg',
+    //   link: '/productos/souvenirs',
+    // },
+    // {
+    //   nombre: 'Artículos Decorativos',
+    //   imagen: 'assets/images/decorativos.jpg',
+    //   link: '/productos/decorativos',
+    // },
+    // {
+    //   nombre: 'Productos Personalizados',
+    //   imagen: 'assets/images/personalizados.jpg',
+    //   link: '/productos/personalizados',
+    // },
   ];
 
   industrias = [
@@ -25,6 +56,10 @@ productos = [
     { nombre: 'Túneles', imagen: 'assets/images/indus_02.jpg', link: '/industrias/tuneles' },
     { nombre: 'Canteras', imagen: 'assets/images/indus_03.jpg', link: '/industrias/canteras' },
     { nombre: 'Pozos de Agua', imagen: 'assets/images/indus_04.jpg', link: '/industrias/pozos' },
-    { nombre: 'Construcción', imagen: 'assets/images/indus_05.jpg', link: '/industrias/construccion' },
+    {
+      nombre: 'Construcción',
+      imagen: 'assets/images/indus_05.jpg',
+      link: '/industrias/construccion',
+    },
   ];
 }
